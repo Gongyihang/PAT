d990156e191365cea3e5b78126071f15129b5f78
Gong_yihang <474356284@qq.com>
2020-04-25 21:49:07 +0800

    4.25

--- 1007.cpp
+++ 1007.cpp
@@ -1,3 +1,47 @@
+// #include <cmath>
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> v(n, 0);
+//     bool flag = false;
+//     for (int i = 0; i < n; i++) {
+//         cin >> v[i];
+//         if (v[i] >= 0)
+//             flag = true;
+//     }
+//     if (!flag) {
+//         cout << "0 " << v[0] << " " << v[n - 1];
+//         return 0;
+//     }
+//     vector<int> dp(n, 0);
+//     dp[0] = v[0];
+//     for (int i = 1; i < n; i++) {
+//         dp[i] = max(dp[i - 1] + v[i], v[i]);
+//     }
+
+//     int index = 0;
+//     for (int i = 0; i < n; i++) {
+//         if (dp[i] > dp[index]) {
+//             index = i;
+//         }
+//     }
+
+//     int max_sum = dp[index], start;
+//     for (start = index; start >= 0; start--) {
+//         max_sum -= v[start];
+//         if (!max_sum)
+//             break;
+//     }
+
+//     cout << dp[index] <<" "<< v[start] <<" "<< v[index];
+//     return 0;
+// }
+
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -7,17 +51,19 @@ int main()
 {
     int n;
     cin >> n;
-    vector<int> v(n, 0);
     bool flag = false;
+    vector<int> v(n, 0);
     for (int i = 0; i < n; i++) {
         cin >> v[i];
         if (v[i] >= 0)
             flag = true;
     }
+
     if (!flag) {
-        cout << "0 " << v[0] << " "<< v[n - 1];
+        cout << "0 " << v[0] << " " << v[n - 1];
         return 0;
     }
+
     vector<int> dp(n, 0);
     dp[0] = v[0];
     for (int i = 1; i < n; i++) {
@@ -31,13 +77,16 @@ int main()
         }
     }
 
-    int max_sum = dp[index], start;
-    for (start = index; start >= 0; start--) {
-        max_sum -= v[start];
-        if (!max_sum)
+    int max_sum = dp[index];
+    int start = 0;
+    for (int i = index; i >= 0; i--) {
+        max_sum -= v[i];
+        if (max_sum == 0) {
+            start = i;
             break;
+        }
     }
 
-    cout << dp[index] <<" "<< v[start] <<" "<< v[index];
+    cout << dp[index] << " " << v[start] << " " << v[index];
     return 0;
 }
