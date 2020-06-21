c5613324b0bed10e7c6e7a876edb10613772a29c
gyh <474356284@qq.com>
2020-06-21 20:23:40 +0800

    6.21

--- 1003i.cpp
+++ 1003i.cpp
@@ -1,21 +1,20 @@
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 
-int main(){
-    char a[100],n;
+int main()
+{
+    char a[100], n;
     gets(a);
-    scanf("%c",&n);
-    int count=0,i;
+    scanf("%c", &n);
+    int count = 0, i;
 
-    for( i = 0;a[i]!='\0';i++){
-        if (a[i]==n)
-        {
+    for (i = 0; a[i] != '\0'; i++) {
+        if (a[i] == n) {
             count++;
         }
-        
     }
-    printf("%d",count);
+    printf("%d", count);
     system("pause");
     return 0;
 }
