ee3a31203e7072a01d2d5d9a643ec53f141908aa
gyh <474356284@qq.com>
2020-06-30 15:25:26 +0800

    6.30

--- 1003i.cpp
+++ 1003i.cpp
@@ -1,20 +1,49 @@
-#include <iostream>
 #include <algorithm>
-#include <vector>
+#include <cmath>
+#include <iostream>
+#include <map>
 #include <string>
-#include <math.h>
+#include <vector>
 using namespace std;
 
-int main(){
+
+/*
+
+AAPAATAAAA
+0123456789
+
+p=2,t=5
+a=2,b=2,c=4
+
+a * b == c
+p * (t - p - 1) == s.size() - t - 1
+
+*/
+
+int main()
+{
     int n;
     cin >> n;
     string s;
-    for(int i = 0;i < n;i++){
+    int p = 0, t = 0;
+    for (int i = 0; i < n; i++) {
         cin >> s;
-        for(int j = 0;j < (int)s.size();j++){
-            
+        map<char, int> m;
+        for (int j = 0; j < (int)s.size(); j++) {
+            m[s[j]]++;
+            if (s[j] == 'P')
+                p = j;
+            if (s[j] == 'T')
+                t = j;
+        }
+
+        if (m.size() == 3 && m['P'] == 1 && m['T'] == 1 && m['A'] != 0 && (t - p) != 1 && p * (t - p - 1) == s.size() - t - 1) {
+            cout << "YES\n";
+        } else {
+            cout << "NO\n";
         }
     }
+
     system("pause");
     return 0;
 }
